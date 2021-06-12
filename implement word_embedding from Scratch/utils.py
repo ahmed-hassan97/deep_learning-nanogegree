70926cda76bc5583963da919a872de92d3b659dc
ahmed hassan <50107057+ahmed-hassan97@users.noreply.github.com>
2021-06-12 13:31:49 +0200

    Add files via upload

---
+++ implement word_embedding from Scratch/utils.py
@@ -0,0 +1,61 @@
+import re
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from  nltk.stem import SnowballStemmer
+
+
+nltk.download('stopwords')
+TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+
+stop_words = stopwords.words("english")
+stemmer = SnowballStemmer("english")
+tokens = []
+
+def preprocess(text):
+    
+    # Replace punctuation with tokens so we can use them in our model
+    text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    # text = text.replace('\n', ' <NEW_LINE> ')
+    text = text.replace(':', ' <COLON> ')
+    for token in text.split():
+        if token not in stop_words:
+                tokens.append(token)
+    text =  " ".join(tokens)
+    words = text.split()
+    
+    # Remove all words with  5 or fewer occurences
+    word_counts = Counter(words)
+    trimmed_words = [word for word in words if word_counts[word] > 5]
+
+    return trimmed_words
+
+
+
+def create_lookup_tables(words):
+    """
+    Create lookup tables for vocabulary
+    :param words: Input list of words
+    :return: A tuple of dicts.  The first dict....
+    """
+    word_counts = Counter(words)
+    # sorting the words from most to least frequent in text occurrence
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
+    int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
+    vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
+
+    return vocab_to_int, int_to_vocab
+
+    
+
